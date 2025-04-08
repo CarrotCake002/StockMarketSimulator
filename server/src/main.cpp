e5a08732353e8c143443321deed160145bff8da4
Pol Siles <pol.siles-polo@epitech.eu>
2025-04-08 21:56:36 +0200

    [ADD] server now manages multiple client connections in a row, still to be checked if it can handle them in parallel

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -3,21 +3,38 @@
 
 #include <iostream>
 #include <string>
+#include <thread>
 
-// Constants
 const std::string SERVER_GREETING = "Hello from server";
+
+void handleConnectedClient(int client_socket) {
+    Message::sendMessage(client_socket, SERVER_GREETING);
+    std::cout << "Message from client: " << Message::receiveMessage(client_socket) << std::endl;
+    SocketManager::closeSocket(client_socket);
+    std::cout << "Client with fd: " << client_socket << " disconnected." << std::endl;
+}
 
 // Function to handle client connections
 void handleClientConnections() {
     int client_socket;
 
     try {
-        ServerConnectionManager connectionManager;
+        ServerConnectionManager server;
 
-        connectionManager.listenForConnections();
-        client_socket = connectionManager.acceptConnection();
-        Message::sendMessage(client_socket, SERVER_GREETING);
-        std::cout << "Message from client: " << Message::receiveMessage(client_socket) << std::endl;
+        server.listenForConnections();
+
+        while (client_socket >= 0) {
+
+            client_socket = server.acceptConnection();
+            if (client_socket < 0) {
+                std::cerr << "Failed to accept connection." << std::endl;
+                continue;
+            }
+            std::thread client_thread(handleConnectedClient, client_socket);
+            client_thread.detach(); // Detach the thread to handle multiple clients
+            std::cout << "Client connected with fd: " << client_socket << std::endl;
+            std::cout << "Waiting for a new connection..." << std::endl;
+        }
     } catch (const std::runtime_error& e) {
         std::cerr << "Runtime Error: " << e.what() << std::endl;
         throw;
