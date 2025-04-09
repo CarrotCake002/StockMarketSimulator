29ba6a60ba9e3cffdcea8718a9cff57d063c1070
Pol Siles <pol.siles-polo@epitech.eu>
2025-04-09 18:35:37 +0200

    [ADD] handling of commands from client to server (help and exit for now)

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,22 +1,31 @@
 #include "ClientConnectionManager.hpp"
+#include "Signals.hpp"
 
 bool inputErrorHandling(int ac, char **av) {
     if (ac > 3) {
         std::cerr << "Usage: " << av[0] << " <IP> <PORT>" << std::endl;
         return true;
     }
-    if (ac == 2 && std::string(av[1]).find_first_not_of("0123456789.") != std::string::npos) {
-        std::cerr << "Invalid IP address format." << std::endl;
+    if (ac == 2 && std::string(av[1]).find_first_not_of(IP_FORMAT) != std::string::npos) {
+        std::cerr << ERROR_INVALID_IP << std::endl;
         return true;
     }
     if (ac == 3 && av[2]) {
         int port = std::stoi(av[2]);
-        if (port < 0 || port > 65535) {
-            std::cerr << "Port number must be between 0 and 65535." << std::endl;
+        if (port < PORT_MIN || port > PORT_MAX) {
+            std::cerr << ERROR_INVALID_PORT << std::endl;
             return true;
         }
     }
     return false;
+}
+
+bool checkExitCommand(std::string command) {
+    return shouldExit || command == SERVER_EXIT_COMMAND;
+}
+
+void handleInput() {
+    
 }
 
 void handleConnection(std::string ip, int port) {
@@ -25,23 +34,37 @@ void handleConnection(std::string ip, int port) {
     std::string serverResponse;
 
     while (true) {
-        std::getline(std::cin, clientCommand);
-        Message::sendMessage(client.getServerSocket(), clientCommand);
-        serverResponse = Message::receiveMessage(client.getServerSocket());
-        std::cout << "Response from server: " << serverResponse << std::endl;
+        try {
+            std::cout << INPUT_PROMPT;
+            std::getline(std::cin, clientCommand);
+            if (clientCommand.empty()) {
+                std::cerr << ERROR << ERROR_INVALID_COMMAND << std::endl;
+                continue;
+            }
+            if (checkExitCommand(clientCommand)) {
+                std::cout << INFO_EXITING_CLIENT << std::endl;
+                break;
+            }
+            Message::sendMessage(client.getServerSocket(), clientCommand);
+            serverResponse = Message::receiveMessage(client.getServerSocket());
+            std::cout << RESPONSE << serverResponse << std::endl;
+        } catch (const std::runtime_error &e) {
+            std::cerr << ERROR << e.what() << std::endl;
+            break;
+        } catch (...) {
+            std::cerr << ERROR_UNEXPECTED << std::endl;
+            throw;
+        }
     }
 }
 
 int main(int ac, char **av) {
+    signal(SIGINT, signalHandler);
     if (inputErrorHandling(ac, av))
         return 84;
     try {
-    handleConnection(std::string(av[1]), std::stoi(av[2]));
-    } catch (const std::runtime_error &e) {
-        std::cerr << "Runtime Error: " << e.what() << std::endl;
-        return 84;
+        handleConnection(std::string(av[1]), std::stoi(av[2]));
     } catch (...) {
-        std::cerr << "An unexpected error occurred." << std::endl;
         return 84;
     }
     return 0;
