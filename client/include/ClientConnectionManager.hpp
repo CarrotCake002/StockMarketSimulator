5ef67b36084d356a26d544dea4c07819a30e827e
Pol Siles <pol.siles-polo@epitech.eu>
2025-04-09 18:36:27 +0200

    [ADD] most text messages into a constant and usage of constants for raw strings

--- client/include/ClientConnectionManager.hpp
+++ client/include/ClientConnectionManager.hpp
@@ -1,30 +1,30 @@
 #ifndef CLIENT_CONNECTION_MANAGER_HPP
 #define CLIENT_CONNECTION_MANAGER_HPP
+
 #include "Manager/SocketManager.hpp"
 #include "Manager/MessageManager.hpp"
 #include "NetworkInformation.hpp"
 
-
 class ClientConnectionManager : public NetworkInformation {
 public:
-    ClientConnectionManager(std::string ip = "127.0.0.1", int port = 8080) : NetworkInformation(port, ip) {
+    ClientConnectionManager(std::string ip = SERVER_IP, int port = SERVER_PORT) : NetworkInformation(port, ip) {
         try {
             server_sock = SocketManager::createSocket();
         } catch (const std::runtime_error &e) {
-            std::cerr << "Socket creation failed: " << e.what() << std::endl;
+            std::cerr << ERROR_SOCKET_CREATION << e.what() << std::endl;
             exit(EXIT_FAILURE);
         }
 
         if (inet_pton(AF_INET, ip.c_str(), &address.sin_addr) <= 0) {
-            std::cerr << "Invalid address" << std::endl;
+            std::cerr << ERROR_INVALID_IP << std::endl;
             exit(EXIT_FAILURE);
         }
 
         if (connect(server_sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
-            std::cerr << "Connection failed" << std::endl;
+            std::cerr << ERROR_CONNECTING << std::endl;
             exit(EXIT_FAILURE);
         }
-        std::cout << "Connected to server at " << ip << ":" << port << std::endl;
+        std::cout << INFO_CONNECTED_TO_SERVER << ip << ":" << port << std::endl;
     }
 
     int getServerSocket() const { return server_sock; }
