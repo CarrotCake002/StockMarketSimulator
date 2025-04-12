6e616bfa79637acf91220945c475940f9ed35219
Pol Siles <pol.siles-polo@epitech.eu>
2025-04-13 01:14:17 +0200

    [MDF] socket_fd naming to avoid confusion with the socket() function

--- common/include/Controller/SocketController.hpp
+++ common/include/Controller/SocketController.hpp
@@ -1,28 +1,28 @@
-#ifndef SOCKET_CONTROLLER_HPP
-#define SOCKET_CONTROLLER_HPP
+#ifndef SOCKET_fd_CONTROLLER_HPP
+#define SOCKET_fd_CONTROLLER_HPP
 
 #include "NetworkInformation.hpp"
 
-class SocketController {
+class Socket_fdController {
 public:
-    virtual ~SocketController() = default;
+    virtual ~Socket_fdController() = default;
 
-    static int createSocket() {
-        int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    static int createSocket_fd() {
+        int socket_fd_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-        if (socket_fd < 0) {
+        if (socket_fd_fd < 0) {
             throw std::runtime_error(ERROR_SOCKET_CREATION);
         }
-        return socket_fd;
+        return socket_fd_fd;
     }
 
-    static void closeSocket(int socket) {
-        if (socket >= 0) {
-            close(socket);
+    static void closeSocket_fd(int socket_fd) {
+        if (socket_fd >= 0) {
+            close(socket_fd);
         } else {
             throw std::runtime_error(ERROR_SOCKET_CLOSING);
         }
     }
 };
 
-#endif // SOCKET_CONTROLLER_HPP
+#endif // SOCKET_fd_CONTROLLER_HPP
